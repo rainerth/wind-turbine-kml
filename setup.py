03cd5bf830e3012a7556be923ee2efe8a52a84d8
charlie9578 <charlie.plumley@gmail.com>
2021-06-14 22:46:46 +0100

    Update setup.py again

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'collada_wt',         # How you named your package folder (MyLib)
   packages = ['collada_wt'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='BSD 3-Clause License',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This library allows you to easily create a 3D collada wind turbine for use in Google Earth Pro',   # Give a short description about your library
   author = 'Charlie Plumley',                   # Type in your name
   author_email = 'charlie.plumley@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/charlie9578/wind-turbine-kml',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/charlie9578/wind-turbine-kml/archive/refs/tags/v0.2-alpha.tar.gz',    # Found under the release version on GitHub
+  download_url = 'https://github.com/charlie9578/wind-turbine-kml/archive/refs/tags/v0.3-alpha.tar.gz',    # Found under the release version on GitHub
   keywords = ['wind turbine', 'collada', 'google earth', 'kml'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pycollada',
